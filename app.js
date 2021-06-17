f45ff11eadda87fd4ba7f5a72e4727c6bc391dac
Tran Anh Tuat <tuattrananh@gmail.com>
2021-06-17 22:01:49 +0700

    final

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ let slides = slider.querySelectorAll('.slide')
 
 let slide_index = 0
 
+let can_slide_play = true
+
 let hero_bgs = [
-    './images/bun-bo-bg.jpg',
+    './images/goi-cuon-bg.jpg',
     './images/pho-ga-bg.jpg',
-    './images/goi-cuon-bg.jpg'
+    './images/bun-bo-bg.jpg'
 ]
 
 showSlide = (index) => {
@@ -23,18 +25,24 @@ nextSlide = () => {
     showSlide(slide_index)
 }
 
-slider.querySelectorAll('.slider-control-item').forEach((item, index) => {
-    item.addEventListener('click', () => {
-        showSlide(index)
-    })
-})
+// pause slide when mouse come in slider
+slider.addEventListener('mouseover', () => can_slide_play = false)
+
+// resume slide when mouse leave out slider
+slider.addEventListener('mouseleave', () => can_slide_play = true)
 
 showSlide(slide_index)
 
+// auto play slide
 // setInterval(() => {
+//     if (!can_slide_play) return
 //     nextSlide()
-// }, 1000);
+// }, 2500);
 
-document.querySelector('#mb-menu-toggle').addEventListener('click', () => {
-    document.querySelector('#main-menu').classList.toggle('active')
+// manual slide select
+slider.querySelectorAll('.slider-control-item').forEach((item, index) => {
+    item.addEventListener('click', () => showSlide(index))
 })
+
+// mobile menu toggle
+document.querySelector('#mb-menu-toggle').addEventListener('click', () => document.querySelector('#main-menu').classList.toggle('active'))
